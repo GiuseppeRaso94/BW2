6a8bb0da123d141ddf7a4ac4cf9d89c1779bec51
GiuseppeRaso94 <peppone2894@gmail.com>
2024-07-19 19:28:51 +0200

    Project finished

---
+++ page3_script.js
@@ -0,0 +1,16 @@
+// const datesBox = document.getElementById("dates-box");
+// const datesBoxInitialPosition = datesBox.offsetTop;
+// window.addEventListener("scroll", (e) => {
+//   if (screen.width > 768) {
+//     datesBox.classList.add("max-width-fixed");
+//   } else {
+//     datesBox.classList.remove("max-width-fixed");
+//   }
+//   if (window.scrollY >= datesBoxInitialPosition - 10 && screen.width > 768) {
+//     datesBox.classList.add("position-fixed", "almost-top");
+//     datesBox.classList.remove("box-shadow");
+//   } else {
+//     datesBox.classList.add("box-shadow");
+//     datesBox.classList.remove("position-fixed", "almost-top");
+//   }
+// });
